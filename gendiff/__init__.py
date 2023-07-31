bd095d26cf102413a16e054ea31139b8f8790e1d
GrigoriyKruchinin <GrigoriyKruchinin>
2023-07-31 10:33:21 +0200

    global func and gitignore update

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,13 +1,6 @@
-from gendiff.generate import (
-    generate, for_add, for_delete, for_unchanged,
-    for_modified, for_nested, format_nested_value
-)
-from gendiff.parse import parse_data
 from gendiff.generate_diff import generate_diff
 
 
 __all__ = (
-    generate, for_add, for_delete, for_unchanged,
-    for_modified, for_nested, format_nested_value,
-    parse_data, generate_diff
+    generate_diff
 )
