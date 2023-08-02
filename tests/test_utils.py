e7cba6aac4b2a64bb3518936d5201cc56a61fcb7
GrigoriyKruchinin <GrigoriyKruchinin>
2023-08-02 08:22:55 +0200

    update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,17 +2,15 @@ import os
 import json
 
 
-def get_input_data(file_name):
-    fixture_path = os.path.join(
-        'tests', 'fixtures', f'{file_name}'
-    )
+def read_file(file_name):
+    fixture_path = os.path.join('tests', 'fixtures', f'{file_name}')
     with open(fixture_path) as file:
-        return json.load(file)
+        return file.read()
+
+
+def get_input_data(file_name):
+    return json.loads(read_file(file_name))
 
 
 def get_expected_result(file_name):
-    fixture_path = os.path.join(
-        'tests', 'fixtures', f'{file_name}'
-    )
-    with open(fixture_path) as file:
-        return file.read()
+    return read_file(file_name)
