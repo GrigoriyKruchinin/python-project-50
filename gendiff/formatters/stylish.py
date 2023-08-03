c012f71a450c6e014e597228d39d473bc611170f
GrigoriyKruchinin <GrigoriyKruchinin>
2023-08-03 15:21:59 +0200

    naming in stylish

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -16,23 +16,23 @@ def format_value(value, spaces_count=0):
 
 def make_stylish_result(diff, spaces_count=2):
     indent = SEPARATOR * spaces_count
-    formatted_diff = []
+    lines = []
     for item in diff:
         key_name = item["name_key"]
         old_value = format_value(item.get("old_value"), spaces_count)
         new_value = format_value(item.get("new_value"), spaces_count)
         if item["action"] == "unchanged":
             current_value = format_value(item.get("value"), spaces_count)
-            formatted_diff.append(f"{indent}{NONE}{key_name}: {current_value}")
+            lines.append(f"{indent}{NONE}{key_name}: {current_value}")
         elif item["action"] == "modified":
-            formatted_diff.append(f"{indent}{DELETE}{key_name}: {old_value}")
-            formatted_diff.append(f"{indent}{ADD}{key_name}: {new_value}")
+            lines.append(f"{indent}{DELETE}{key_name}: {old_value}")
+            lines.append(f"{indent}{ADD}{key_name}: {new_value}")
         elif item["action"] == "deleted":
-            formatted_diff.append(f"{indent}{DELETE}{key_name}: {old_value}")
+            lines.append(f"{indent}{DELETE}{key_name}: {old_value}")
         elif item["action"] == "added":
-            formatted_diff.append(f"{indent}{ADD}{key_name}: {new_value}")
+            lines.append(f"{indent}{ADD}{key_name}: {new_value}")
 
-    formatted_string = '\n'.join(formatted_diff)
+    formatted_string = '\n'.join(lines)
     end_indent = SEPARATOR * (spaces_count - 2)
 
     return f"{{\n{formatted_string}\n{end_indent}}}"
