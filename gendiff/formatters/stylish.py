8bc1e924d9af7a9ca234a800c7aa6ba8a3d6c876
GrigoriyKruchinin <GrigoriyKruchinin>
2023-08-03 11:13:07 +0200

    linter

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,7 +1,7 @@
 SEPARATOR = " "
 ADD = '+ '
 DELETE = '- '
-UNCHANGED = '  '
+NONE = '  '
 
 
 def format_value(value, spaces_count):
@@ -23,7 +23,7 @@ def make_stylish_result(diff, spaces_count=2):
         new_value = format_value(item.get("new_value"), spaces_count)
         if item["action"] == "unchanged":
             current_value = format_value(item.get("value"), spaces_count)
-            formatted_diff.append(f"{indent}{UNCHANGED}{key_name}: {current_value}")
+            formatted_diff.append(f"{indent}{NONE}{key_name}: {current_value}")
         elif item["action"] == "modified":
             formatted_diff.append(f"{indent}{DELETE}{key_name}: {old_value}")
             formatted_diff.append(f"{indent}{ADD}{key_name}: {new_value}")
